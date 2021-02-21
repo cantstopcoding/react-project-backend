004cc455edb6741cb889b5811a5d237b4546f443
cantstopcoding <jonathan_koss@yahoo.com>
2021-02-21 12:50:42 -0500

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+account = Account.create(name: "Checking", balance: 1000)
+
+transaction = Transaction.create(account_id: 1, amount: 10, kind: "deposit", date: Date.today, description: "Found $10")
+
+transaction_two = Transaction.create(account_id: 1, amount: 10, kind: "withdraw", date: Date.today, description: "Taking $10")
