36faec2b611cf18d107248b3b9870c9a524274b9
cantstopcoding <jonathan_koss@yahoo.com>
2021-02-21 11:59:35 -0500

    namespace routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :transactions
-  resources :accounts
+  namespace :api do 
+    namespace :v1 do 
+      resources :transactions
+      resources :accounts
+    end
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
