0df7da353d4ff350bf37087c372fdc366371aa68
cantstopcoding <jonathan_koss@yahoo.com>
2021-02-21 13:53:31 -0500

    create update balance method

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,17 @@
 class Account < ApplicationRecord
     validates :name, :balance, presence: true
+
+    def update_balance
+        if transaction.kind == 'deposit'
+            self.balance += transaction.amount
+            self.save
+        elsif transaction.kind == 'withdraw'
+            if self.balance >= transaction.amount
+                self.balance -= transaction.amount
+                self.save
+            else
+                return 'Balance is too low. '
+            end
+        end
+    end
 end
