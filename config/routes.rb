c2c071979d4e62018e33a3b00252cc68ac40a3ca
cantstopcoding <jonathan_koss@yahoo.com>
2021-02-22 17:30:42 -0500

    nest transactions to accounts in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
-      resources :transactions
-      resources :accounts
+      resources :accounts do 
+        resources :transactions
+      end
     end
   end
-  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
