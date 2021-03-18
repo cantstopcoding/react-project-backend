d6ed0ff632ae0f930fbfebaad7ff1a5a138b6080
cantstopcoding <jonathan_koss@yahoo.com>
2021-03-18 17:39:49 -0400

    create associations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,20 @@
 class Item < ApplicationRecord
+    has_many :transactions 
+    has_many :accounts, through: :transactions
 end
+
+
+#   class Physician < ApplicationRecord
+#     has_many :appointments
+#     has_many :patients, through: :appointments
+#   end
+  
+#   class Appointment < ApplicationRecord
+#     belongs_to :physician
+#     belongs_to :patient
+#   end
+  
+#   class Patient < ApplicationRecord
+#     has_many :appointments
+#     has_many :physicians, through: :appointments
+#   end
