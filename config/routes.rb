dda94efcec9d676aa9a95dabd8804187cdf6d3a0
cantstopcoding <jonathan_koss@yahoo.com>
2021-03-21 13:06:47 -0400

    crete api/v1/items/transactions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :items
   namespace :api do 
     namespace :v1 do 
       resources :accounts do 
+        resources :transactions
+      end
+      resources :items do 
         resources :transactions
       end
     end
