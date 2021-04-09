fc202477c6eb12928ef2cb853ca119fad2bc674c
cantstopcoding <jonathan_koss@yahoo.com>
2021-04-09 11:12:05 -0400

    update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,12 +12,12 @@ Item.destroy_all
 checkings_account = Account.create(name: "Checking", balance: 1000)
 savings_account = Account.create(name: "Savings", balance: 5000)
 
-# outlander_dress  = Item.create(name: "Outlander Costume", image_url: "https://yt3.ggpht.com/a/AATXAJx5mEunDOUJudzpBNqD61pppw8kcQvgmDHMosko=s100-c-k-c0xffffffff-no-rj-mo", price: 500.00, description:"It's awesome!")
-# expensive_purse = Item.create(name: "Expensive Purse", image_url: "https://cdn.shopify.com/s/files/1/0384/0161/products/56265-92_20Chanel_20Vintage_20Classic_20Double_20Flap_20Bag_20Qu_2D_0002_518x518.jpg?v=1591021463", price: 100000.00, description:"It's awesome!")
-# expensive_sandals = Item.create(name: "Expensive Sandals", image_url: "https://www.mybudapester.com/media/catalog/product/cache/2/image/900x900/9df78eab33525d08d6e5fb8d27136e95/i/m/image_3681_1_5394.jpg", price: 5000000000000.54, description: "It's too much damn money", category_id: sandals.id)
+outlander_dress  = Item.create(account_id: checkings_account.id, name: "Outlander Costume", image_url: "https://yt3.ggpht.com/a/AATXAJx5mEunDOUJudzpBNqD61pppw8kcQvgmDHMosko=s100-c-k-c0xffffffff-no-rj-mo", price: 500.00, description:"It's awesome!")
+expensive_purse = Item.create(account_id: savings_account.id, name: "Expensive Purse", image_url: "https://cdn.shopify.com/s/files/1/0384/0161/products/56265-92_20Chanel_20Vintage_20Classic_20Double_20Flap_20Bag_20Qu_2D_0002_518x518.jpg?v=1591021463", price: 100000.00, description:"It's awesome!")
+expensive_sandals = Item.create(account_id: checkings_account.id, name: "Expensive Sandals", image_url: "https://www.mybudapester.com/media/catalog/product/cache/2/image/900x900/9df78eab33525d08d6e5fb8d27136e95/i/m/image_3681_1_5394.jpg", price: 5000000000000.54, description: "It's too much damn money")
 
-item = Item.create(account_id: checkings_account.id, amount: 10, kind: "deposit", date: Date.today, description: "Found $10")
+# item = Item.create(account_id: checkings_account.id, amount: 10, kind: "deposit", date: Date.today, description: "Found $10")
 
-item_two = Item.create(account_id: checkings_account.id, amount: 10, kind: "withdraw", date: Date.today, description: "Taking $10")
+# item_two = Item.create(account_id: checkings_account.id, amount: 10, kind: "withdraw", date: Date.today, description: "Taking $10")
 
 
