318399d211e7e9a8e89782699d840fca3bc7205d
cantstopcoding <jonathan_koss@yahoo.com>
2021-04-11 16:53:00 -0400

    create admin model

---
+++ app/models/admin.rb
@@ -0,0 +1,32 @@
+class Admin < ApplicationRecord
+  has_many :items
+  validates :first_name, :last_name, :username, :email, :image_url, presence: true
+
+  # def update_balance(item)
+  #     if item.kind == 'deposit'
+  #         self.balance += item.amount
+  #         self.save
+  #     elsif item.kind == 'withdraw'
+  #         if self.balance >= item.amount
+  #             self.balance -= item.amount
+  #             self.save
+  #         else
+  #             return 'Balance too low.'
+  #         end
+  #     end
+  # end
+
+  # def update_balance_on_delete(item)
+  #     if item.kind == 'deposit'
+  #         if self.balance >= item.amount
+  #             self.balance -= item.amount
+  #             self.save
+  #         else
+  #             return 'Balance is too low.'
+  #         end
+  #     elsif item.kind == 'withdraw'
+  #         self.balance += item.amount
+  #         self.save
+  #     end
+  # end
+end
