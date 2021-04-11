d587552b6255fb8f441d7a650b97d7abd047582b
cantstopcoding <jonathan_koss@yahoo.com>
2021-04-11 17:16:13 -0400

    make admins route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
-      resources :accounts do 
+      resources :admins do 
         resources :items
       end
     end
