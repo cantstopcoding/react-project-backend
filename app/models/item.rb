a1e13e69662d59594ec4fc9246d2e8d6e6cb7f30
cantstopcoding <jonathan_koss@yahoo.com>
2021-04-11 16:54:02 -0400

    make belongs to admin

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-  belongs_to :account
+  belongs_to :admin
   validates :name, :image_url, :description, :price, presence: true
   # validates_inclusion_of :kind, :in => ["deposit", "withdraw"]
 end
