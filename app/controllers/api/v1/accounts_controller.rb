12f1ea9ecf17e53a515eb965db3726c192478976
cantstopcoding <jonathan_koss@yahoo.com>
2021-10-02 11:29:55 -0400

    order account in api by fist name in alphabetical order

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::AccountsController < ApplicationController
 
   def index
     @accounts = Account.all
-    render json: @accounts
+    render json: @accounts.alpha
   end
 
   def show
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,6 @@
 class Account < ApplicationRecord
   has_many :items, dependent: :destroy
   validates :first_name, :last_name, :username, :email, presence: true
+
+  scope :alpha, -> { (order(:first_name)) }
 end
